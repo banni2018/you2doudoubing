0153b9c00676f3a5907e2a7af0a01257cc4fd1e6
banni2018 <42250413+banni2018@users.noreply.github.com>
2018-08-31 09:31:59 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyARHJJDFtwb5wcnURwwJA3FQRrM16PF27c');
+define('APIKEY', 'AIzaSyCnTBn425-z5RxpqQu06TYeUwUnqG4BLLc');
 define('GJ_CODE', 'JP');
 define('SITE_NAME', '大爷常来玩儿啊');
 define('TITLENAME', 'DouDouTube');
 define('EN2DEKEY', 'lujianbupingyishenghou');
-define('EMAIL', 'xiaofufua@protonmail.com');
+define('EMAIL', 'zhikai618@gnmail.com');
 ?>
